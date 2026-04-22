e68f14748dec03e991071cde3be005eee1d6cbc9
Joelciomatias <joelcio_psx@hotmail.com>
2026-04-21 21:42:02 -0300

    Implement project and task management API with CRUD operations

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 from fastapi import FastAPI
-from typing import Optional
-from pydantic import BaseModel
-from datetime import date, datetime, time, timedelta
-from backend.model import Project, Task
+
+from backend.api import router
+from backend.db import create_tables
 
 app = FastAPI()
 
 
+@app.on_event("startup")
+def startup():
+    create_tables()
 
 
-@app.post("/projects")
-def create_project(project: Project):
-    return project
-
-@app.get("/projects/{id}")
-def read_project(id: int, q: str | None = None):
-    return {"id": id, "q": q}
-
-@app.put("/projects/{project_id}")
-async def update_project(project_id: int, project: Project):
-    return {"project_id": project_id, **project.model_dump()}
-
-@app.delete("/projects/{project_id}")
-async def delete_project(project_id: int):
-    return {"project_id": project_id, "message": "Project deleted successfully"}
+app.include_router(router)
